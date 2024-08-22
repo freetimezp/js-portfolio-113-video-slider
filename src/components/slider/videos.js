f849d9593bba03108131c11686fa0ef44beabf8d
freetimezp <kraftukrnet@ukr.net>
2024-08-22 21:02:13 +0300

    02. js-portfolio-113-video-slider. Main UI.

---
+++ src/components/slider/videos.js
@@ -0,0 +1,26 @@
+export const videos = [
+    {
+        id: "711863471",
+        title: "Cineprint",
+        category: "Documentary",
+        date: "May 2022",
+    },
+    {
+        id: "478246234",
+        title: "Life style",
+        category: "Sci-Fi",
+        date: "June 2022",
+    },
+    {
+        id: "387407107",
+        title: "Origin",
+        category: "Art",
+        date: "July 2022",
+    },
+    {
+        id: "704562417",
+        title: "Rutina",
+        category: "Nature",
+        date: "August 2022",
+    },
+];
